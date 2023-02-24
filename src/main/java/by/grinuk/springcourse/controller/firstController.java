5a3fa9d545927bee10b3291d989a8628d23965c8
ZakviS <lesha.grinuk@gmail.com>
2023-02-24 20:58:19 +0300

    20 lesson

--- src/main/java/by/grinuk/springcourse/controller/firstController.java
+++ src/main/java/by/grinuk/springcourse/controller/firstController.java
@@ -1,6 +1,7 @@
 package by.grinuk.springcourse.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.HttpSessionRequiredException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,5 +21,35 @@ public class firstController {
     @GetMapping("/goodbye")
     public String goodbyePage(){
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculation")
+    public String calculation (@RequestParam(value = "a") int a,
+                               @RequestParam(value = "b") int b,
+                               @RequestParam(value = "action") String action,Model model){
+
+        double answer;
+        switch (action) {
+            case  "multiplication":
+                answer=a*b;
+                break;
+            case "addition":
+                answer=a+b;
+                break;
+            case ("subtraction"):
+                answer=a-b;
+                break;
+            case ("division"):
+                answer=a/(double)b;
+                break;
+            default:
+                answer = 0;
+                break;
+        }
+
+        model.addAttribute("message", answer);
+
+
+        return "first/calculation";
     }
 }
