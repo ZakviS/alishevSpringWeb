9504ae61e0efce656742c5439b7ae8c4b21db7a4
ZakviS <lesha.grinuk@gmail.com>
2023-02-26 20:13:40 +0300

    23 lesson

--- src/main/java/by/grinuk/springcourse/controller/peopleController.java
+++ src/main/java/by/grinuk/springcourse/controller/peopleController.java
@@ -2,7 +2,7 @@ package by.grinuk.springcourse.controller;
 
 import by.grinuk.springcourse.dao.PersonDAO;
 import by.grinuk.springcourse.models.Person;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -39,6 +39,24 @@ public class peopleController {
     @PostMapping()
     public String create(@ModelAttribute("person") Person person){
         personDAO.save(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model,@PathVariable("id") int id){
+        model.addAttribute("person", personDAO.show(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id){
+        personDAO.update(id,person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id){
+        personDAO.delete(id);
         return "redirect:/people";
     }
 }
