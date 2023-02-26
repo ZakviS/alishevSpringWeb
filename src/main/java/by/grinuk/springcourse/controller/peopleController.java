34ecc765318df014b1c3d746f8a8661ed6590385
ZakviS <lesha.grinuk@gmail.com>
2023-02-26 14:57:13 +0300

    22 lesson

---
+++ src/main/java/by/grinuk/springcourse/controller/peopleController.java
@@ -0,0 +1,44 @@
+package by.grinuk.springcourse.controller;
+
+import by.grinuk.springcourse.dao.PersonDAO;
+import by.grinuk.springcourse.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/people")
+public class peopleController {
+
+
+    public peopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    private final PersonDAO personDAO;
+
+    @GetMapping()
+    public String index(Model model){
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model){
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(Model model){
+        model.addAttribute("person",new Person());
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person){
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+}
---
+++ src/main/java/by/grinuk/springcourse/dao/PersonDAO.java
@@ -0,0 +1,39 @@
+package by.grinuk.springcourse.dao;
+
+import by.grinuk.springcourse.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+
+    private static int ID;
+
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(ID,"Tom"));
+        ID++;
+        people.add(new Person(ID,"Jack"));
+        ID++;
+        people.add(new Person(ID,"Bob"));
+        ID++;
+    }
+
+    public List<Person> index(){
+        return people;
+    }
+
+    public Person show(int id){
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person){
+        person.setId(++ID);
+        people.add(person);
+    }
+}
